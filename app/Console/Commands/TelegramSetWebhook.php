aba7631b7b79f93607224c3f92d7d569c9df078a
Cell6969 <bossmarinoo@gmail.com>
2026-07-05 15:56:36 +0700

    feat: add integration with bot telegram

---
+++ app/Console/Commands/TelegramSetWebhook.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Attributes\Description;
+use Illuminate\Console\Attributes\Signature;
+use Illuminate\Console\Command;
+use Telegram\Bot\Laravel\Facades\Telegram;
+
+#[Signature('app:telegram-set-webhook')]
+#[Description('Command description')]
+class TelegramSetWebhook extends Command
+{
+    /**
+     * Execute the console command.
+     */
+    public function handle()
+    {
+        Telegram::setWebhook([
+            'url' => config('telegram.bots.mybot.webhook_url')
+        ]);
+
+        $this->info('Telegram webhook set successfully.');
+    }
+}
